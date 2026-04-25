7e51754b3bf5097a0acd294c1f7b81508d9dc29f
ajayg3997 <ajayg3997@gmail.com>
2026-04-25 15:34:42 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'devops-engineer-with-react',
+  base: '/devops-engineer-with-react/',
   plugins: [react()],
 })
